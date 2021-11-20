e037fd91ed20d789f866b15dc78f95505485fc87
TinaKrisss <kriskisina@gmail.com>
2021-11-20 21:30:37 +0200

    Added Card Class

---
+++ CardGame/Card.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Card
+{
+	public string CardSuit { get; set; }
+	public int CardType { get; set; }
+
+	public Card(string suit, int type)
+	{
+		CardSuit = suit;
+		CardType = type;
+	}
+}
