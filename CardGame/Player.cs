211e6add1d1c550573d4b159f18704338c6469b2
TinaKrisss <kriskisina@gmail.com>
2021-11-20 21:55:24 +0200

    Added Class Player

---
+++ CardGame/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Player
+{
+	public string PlayerName
+	{
+		get; 
+		set
+		{
+			if (value.Length != 0)
+			{
+				PlayerName = value;
+			};
+		} 
+	}
+	public Stack<Card> PlayerCards { get; set; }
+
+	public Player(string name, Stack cards)
+	{
+		PlayerName = name;
+		PlayerCards = cards;
+	}
+}
